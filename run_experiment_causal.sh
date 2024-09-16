2a93c9a544f1736777b0a27e457e71e7f165e57c
Giovanni Briglia <93917046+Giovannibriglia@users.noreply.github.com>
2024-09-16 10:14:35 +0200

    simulation give_way

--- run_experiment_causal.sh
+++ run_experiment_causal.sh
@@ -9,18 +9,18 @@
 # Activate the virtual environment
 source MyEnv/bin/activate
 
-# Set the display environment variable
-export DISPLAY=':99.0'
-
-# Start a virtual framebuffer in the background
-Xvfb :99 -screen 0 1400x900x24 > /dev/null 2>&1 &
-
 # Install the package
 python setup.py install
 
 # Change directory to examples/running
 # shellcheck disable=SC2164
 cd examples/running
+
+# Set the display environment variable
+export DISPLAY=':99.0'
+
+# Start a virtual framebuffer in the background
+Xvfb :99 -screen 0 1400x900x24 > /dev/null 2>&1 &
 
 # Run the experiment using Python 3.10
 python3.10 -m run_experiment_causal
