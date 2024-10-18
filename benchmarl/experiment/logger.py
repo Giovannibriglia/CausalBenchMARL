630c1ddbbde914f58c8b3d6b80010cbec98e0188
Giovanni Briglia <93917046+Giovannibriglia@users.noreply.github.com>
2024-10-18 17:51:15 +0200

    change wandb project

--- benchmarl/experiment/logger.py
+++ benchmarl/experiment/logger.py
@@ -62,7 +62,7 @@ class Logger:
                     experiment_name=experiment_name,
                     wandb_kwargs={
                         "group": task_name,
-                        "project": "benchmarl",
+                        "project": "benchmarl_single_agent",
                         "id": experiment_name,
                     },
                 )
